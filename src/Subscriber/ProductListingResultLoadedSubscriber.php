c6020fd5d74465f5c73aa65a83f1ed0fa9256827
Daniel Sturm <d.sturm@zirkeldesign.de>
2022-05-05 16:11:29 +0200

    feat: :sparkles: Show variants in product listing on search results

--- src/Subscriber/ProductListingResultLoadedSubscriber.php
+++ src/Subscriber/ProductListingResultLoadedSubscriber.php
@@ -1,13 +1,16 @@
-<?php declare(strict_types=1);
+<?php
+
+declare(strict_types=1);
 
 namespace SasVariantSwitch\Subscriber;
 
 use SasVariantSwitch\SasVariantSwitch;
-use Shopware\Core\Content\Product\Events\ProductListingResultEvent;
 use Shopware\Core\Content\Product\ProductCollection;
 use Shopware\Core\System\SystemConfig\SystemConfigService;
-use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use SasVariantSwitch\Storefront\Event\ProductBoxLoadedEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Shopware\Core\Content\Product\Events\ProductSearchResultEvent;
+use Shopware\Core\Content\Product\Events\ProductListingResultEvent;
 use SasVariantSwitch\Storefront\Page\ProductListingConfigurationLoader;
 
 class ProductListingResultLoadedSubscriber implements EventSubscriberInterface
@@ -28,6 +31,9 @@ class ProductListingResultLoadedSubscriber implements EventSubscriberInterface
         return [
             // 'sales_channel.product.loaded' => 'handleProductListingLoadedRequest',
             ProductListingResultEvent::class => [
+                ['handleProductListingLoadedRequest', 201],
+            ],
+            ProductSearchResultEvent::class => [
                 ['handleProductListingLoadedRequest', 201],
             ],
             ProductBoxLoadedEvent::class => [
